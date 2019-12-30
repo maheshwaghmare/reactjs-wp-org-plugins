11e40adfea768f728a2abc040c8b16b25c943368
maheshwaghmare <mwaghmare7@gmail.com>
2019-12-31 03:19:53 +0530

    Updates

---
+++ precache-manifest.1583c95971b538d425557ecf2d07623d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aac1542e11c74de15af7d092095655a8",
+    "url": "/reactjs-wp-org-plugins/index.html"
+  },
+  {
+    "revision": "1b1650b3c065ef628fcc",
+    "url": "/reactjs-wp-org-plugins/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "a8559e0caa4481d240c1",
+    "url": "/reactjs-wp-org-plugins/static/css/main.ba11474e.chunk.css"
+  },
+  {
+    "revision": "1b1650b3c065ef628fcc",
+    "url": "/reactjs-wp-org-plugins/static/js/2.6fbaf0ba.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/reactjs-wp-org-plugins/static/js/2.6fbaf0ba.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a8559e0caa4481d240c1",
+    "url": "/reactjs-wp-org-plugins/static/js/main.cf80a03f.chunk.js"
+  },
+  {
+    "revision": "f2f464a77ba961745819",
+    "url": "/reactjs-wp-org-plugins/static/js/runtime-main.8622a118.js"
+  }
+]);
